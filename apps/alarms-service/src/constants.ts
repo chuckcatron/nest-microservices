7a4829bb1d9bf8847e749b24664c32df61f0afd7
Chuck Catron <charles.catron@gmail.com>
2024-06-28 16:23:45 -0400

    message acknowledgement

--- apps/alarms-service/src/constants.ts
+++ apps/alarms-service/src/constants.ts
@@ -1,1 +1,2 @@
-export const MESSAGE_BROKER = 'message-broker';
+export const NATS_MESSAGE_BROKER = Symbol('nats-message-broker');
+export const NOTIFICATIONS_SERVICE = Symbol('notifications-service');
